5ca26cb12cb481a9c3c6256b7cc0255dbfe2ca4c
pdaware <76773941+pdaware@users.noreply.github.com>
2023-07-30 18:36:12 +0530

    All Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,58 @@
-import { Component } from '@angular/core';
-import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: true,
-    navSpeed: 700,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1,
-      },
-      400: {
-        items: 1,
-      },
-      740: {
-        items: 2,
-      },
-      940: {
-        items: 2,
-      },
-    },
-    nav: true,
-  };
+export class AppComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
+
+  isFormSubmit: Boolean = false;
+
+  isPopup: boolean = false;
+  contactForm!: FormGroup;
 
   growYourBusiness: any[] = [
     {
+      ICON: 'fa fa-smile-o',
       TITLE: 'Struggling',
       MAIN: 'Are You Struggling To Generate Quality Leads In Your Business On Regular Basis?',
     },
     {
+      ICON: 'fa fa-smile-o',
       TITLE: 'Worry',
       MAIN: 'Are You Worried About Competition To Deal With Low Price Competitors?',
     },
     {
+      ICON: 'fa fa-smile-o',
       TITLE: 'Sale',
       MAIN: 'Are You Worried That Your Business Is Stagnated And No Predictable Sale?',
     },
     {
+      ICON: 'fa fa-smile-o',
       TITLE: 'Frustrated',
       MAIN: 'Are You Frustrated That Your Sales Team Is Not Having Enough Training On Sales To Convert Leads Into Sales?',
     },
     {
+      ICON: 'fa fa-smile-o',
       TITLE: 'Lack Of Knowledge',
       MAIN: 'Are You Worried About The Lack Of Knowledge On How To Get Your Employees To Give Their Best?',
     },
     {
+      ICON: 'fa fa-smile-o',
       TITLE: 'Money Stuck',
       MAIN: 'Are You Worried About The Money Stuck In The Market?',
     },
     {
+      ICON: 'fa fa-smile-o',
       TITLE: 'Roadmap',
       MAIN: 'Worried About Not Having The Clear Roadmap On How To Proceed From Here To Take It To Next Level.',
     },
@@ -112,6 +107,18 @@ export class AppComponent {
     },
   ];
 
+  ngOnInit() {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+    });
+  }
+
+  get formValidationStateContactUs() {
+    return this.contactForm.controls;
+  }
+
   onClickSocial(socilaType: string) {
     if (socilaType === 'facebook') {
       window.open('https://facebook.com/subhashwangdebusinesscoach', '_blank');
@@ -121,6 +128,52 @@ export class AppComponent {
       window.open('https://www.instagram.com/subhaway', '_blank');
     } else if (socilaType === 'youtube') {
       window.open('https://www.youtube.com/subhaway', '_blank');
+    }
+  }
+
+  openPopup() {
+    this.isPopup = true;
+  }
+  closePopup() {
+    this.isPopup = false;
+  }
+  submitForm() {
+    this.isFormSubmit = true;
+    if (this.contactForm.valid) {
+      // console.log(this.contactForm.value);
+      // handle the form submission logic here
+
+      // Replace with your actual EmailJS service ID and template ID
+      const serviceID = 'subhashwangde';
+      const templateID = 'subhashTemplateID';
+
+      // Replace with your actual EmailJS user ID
+      const userID = 'P9p7sPU9uoAC_5a6X';
+
+      // Format the data to be sent via EmailJS
+      const templateParams = {
+        from_name: this.contactForm.value.name,
+        from_email: this.contactForm.value.email,
+        from_mobile: this.contactForm.value.mobile,
+      };
+
+      // Send the email using EmailJS
+      emailjs.send(serviceID, templateID, templateParams, userID).then(
+        (response: EmailJSResponseStatus) => {
+          // console.log('Email sent successfully:', response);
+          alert('Form submitted successfully!');
+          this.contactForm.reset();
+          // Optionally, show a success message to the user
+        },
+        (error) => {
+          console.error('Error sending email:', error);
+          // Optionally, show an error message to the user
+        }
+      );
+      this.closePopup();
+    } else {
+      // show an error message or handle the form validation errors
+      // console.log('Form is not valid');
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CarouselModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
